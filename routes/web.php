4b910f8ebe4cf3bc9c7db779f91e64c29698df1c
jkonini15 <jkonini15@epoka.edu.al>
2020-05-15 21:35:39 +0200

    aa

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,9 @@
 |
 */
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+Route::get('/', function () {
+    return view('welcome');
+});
 
 //Route::get('/test', function () {
 //    return "Welcome to test, routes okay";
